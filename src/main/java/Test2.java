6118966eac7bd5b7fd7311d8d6e87adcbee7bd8b
victor.klymenko <victor.klymenko@engagepoint.com>
2013-11-15 12:14:37 +0200

    New file

---
+++ src/main/java/Test2.java
@@ -0,0 +1,11 @@
+package main.java;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: victor.klymenko
+ * Date: 11/15/13
+ * Time: 12:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test2 {
+}
