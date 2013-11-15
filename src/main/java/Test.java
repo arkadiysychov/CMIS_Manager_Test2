2e1424933f00e47d671752f13df43205c0b5c3a4
arkadiy.sychov <arkadiy.sychov@engagepoint.com>
2013-11-15 11:42:10 +0200

    TB-20 Test commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: arkadiy.sychov
+ * Date: 11/15/13
+ * Time: 11:39 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
